74b66da0fc29a677ae31a02601b75054c623fccd
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-08-02 13:27:51 -0300

    Finalizando as plataformas

--- Scripts/FallingPlatform.gd
+++ Scripts/FallingPlatform.gd
@@ -8,7 +8,7 @@ onready var reset_position = global_position
 var velocity = Vector2.ZERO
 var gravity = 720
 var is_triggered = false
-export var reset_timer = 3.0
+export var reset_timer = 5.0
 
 func _ready():
 	set_physics_process(false)
@@ -16,3 +16,23 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	position += velocity * delta
+	
+func collide_with(_collision: KinematicCollision2D, _collider: KinematicBody2D):
+	if !is_triggered:
+		is_triggered = true
+		anim.play("shake")
+		velocity = Vector2.ZERO
+func _on_anim_animation_finished(anim_name):
+	set_physics_process(true)
+	timer.start(reset_timer)
+
+
+func _on_timer_timeout():
+	set_physics_process(false)
+	yield(get_tree(), "physics_frame")
+	var temp = collision_layer
+	collision_layer = 0
+	global_position = reset_position
+	yield(get_tree(), "physics_frame")
+	collision_layer = temp
+	is_triggered = false
