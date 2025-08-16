96ef415c0905ce27202e5f4426ee8fc82e0e647d
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-08-16 12:53:11 -0300

    Plataformas Moveis 2

--- Scripts/MovingPlatform.gd
+++ Scripts/MovingPlatform.gd
@@ -1,2 +1,19 @@
 extends Node2D
 
+onready var platform = $platform
+onready var tween = $Tween
+
+export var speed = 3.0
+export var horizontal = true
+export var distance = 192
+
+const WAIT_DURATION = 1.0
+
+func _start_tween():
+	var move_direction = Vector2.RIGHT * distance if horizontal else Vector2.UP * distance
+	var duration = move_direction.lenght() / float(speed * 16)
+	
+	tween.interpolate_property(
+		platform, "position", Vector2.ZERO, move_direction, duration, 
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, WAIT_DURATION
+	)
